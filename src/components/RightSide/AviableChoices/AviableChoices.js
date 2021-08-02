c5eaaa6b1c91f291e8d348778f539ccc669bae46
MiloPeregrin <milanperegrin@gmail.com>
2021-08-02 11:53:39 +0200

    Reafactoring AviableChoices komponentu
    Zpřehlednění AviableChoices.js přesunutím return() do samostatného komponentu OptionsItem.js

--- src/components/RightSide/AviableChoices/AviableChoices.js
+++ src/components/RightSide/AviableChoices/AviableChoices.js
@@ -1,31 +1,23 @@
 import React, { useContext } from "react";
-import { StateContext } from "../store/StateContext";
-import { DataContext } from "../store/DataContext";
+import { StateContext } from "../../store/StateContext";
+import { DataContext } from "../../store/DataContext";
+import OptionsItem from "./OptionsItem";
 
 const AviableChoices = (props) => {
   const { node, setNode } = useContext(StateContext);
   const textNodes = useContext(DataContext);
 
-  function renderButtons(textNodeIndex) {
+  function renderOptions(textNodeIndex) {
     const textNode = textNodes.find(
       (textNode) => textNode.id === textNodeIndex
     );
     const options = textNode.options;
-
-    return (
-      <React.Fragment>
-        {options.map((item) => (
-          <li key={Math.random()}>
-            <a href="#">{item.text}</a>
-          </li>
-        ))}
-      </React.Fragment>
-    );
+    return <OptionsItem options={options} />;
   }
 
   return (
     <div className="row aviable-choices borderline">
-      <ul>{renderButtons(node)}</ul>
+      <ul>{renderOptions(node)}</ul>
     </div>
   );
 };
